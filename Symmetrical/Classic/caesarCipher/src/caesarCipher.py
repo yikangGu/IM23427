5a66b74e2c6eaa71aec9e39c1352178495dab637
yikangGu <yikangGu@github.com>
2018-09-25 17:43:41 +0800

    update caesarCipher

--- Symmetrical/Classic/caesarCipher/src/caesarCipher.py
+++ Symmetrical/Classic/caesarCipher/src/caesarCipher.py
@@ -1,22 +1,39 @@
 import random
 
 key = random.randint(1, 25)
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-symbolIdx = []
 
 
-def isAlphabet(squence):
-    symbolIdx = [i for i in range(len(squence))]
-
-
-def Encrypto(sequence, key):
-    cryptotext = [ord(i) + key for i in sequence]
+def caesarCipher(sequence, key, mode="encrypto"):
+    # 65 is the acsii code of the char "A"
+    # 97 is "a"'s
+    text = []
+    for i in range(len(sequence)):
+        if not sequence[i].isalpha():
+            text.append(sequence[i])
+        else:
+            if ord(sequence[i]) <= 90:
+                sub = 65
+            else:
+                sub = 97
+            if mode == "encrypto":
+                text.append(chr((ord(sequence[i]) - sub + key) % 26 + sub))
+            if mode == "decrypto":
+                text.append(chr((ord(sequence[i]) - sub - key) % 26 + sub))
+    return "".join(text)
 
 
 def main():
-    k = "a"
-    print(ord("a"), ord("A"), ord("z"), ord("Z"))
-    print()
+    text = "E:/Users/DELL/Anaconda3/envs/ame/python.exe\
+    f:/Privacy/ProgramFile/Github/Cryptology/Symmetrical/Classic\
+    /caesarCipher/src/caesarCipher.py"
+
+    print("key : " + str(key))
+    print("plainText : " + text)
+    encryptoText = caesarCipher(text, key, mode="encrypto")
+    print("encryptoText : " + encryptoText)
+    decryptoText = caesarCipher(encryptoText, key, mode="decrypto")
+    print("decryptoText : " + decryptoText)
+
 
 if __name__ == '__main__':
     main()
