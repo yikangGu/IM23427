bdeeb14969e74cffe29744519aa207f8a0b06515
yikangGu <yikangGu@github.com>
2018-10-02 19:39:20 +0800

    fix rtc

--- Symmetrical/Classic/Row Transposition Cipher/src/RTC.py
+++ Symmetrical/Classic/Row Transposition Cipher/src/RTC.py
@@ -4,15 +4,15 @@ import numpy as np
 def RTC(tx, key):
     keyStr = str(key)
     keyLenth = len(keyStr)
-    splitTx = [tx[n * 5: (n + 1) * 5] for n in range(int(len(tx) / keyLenth))]
+    splitTx = [tx[n * keyLenth: (n + 1) * keyLenth] for n in range(int(len(tx) / keyLenth))]
     cipherTx = ''.join([j[int(i) - 1] for i in keyStr for j in splitTx])
     return cipherTx
 
 
 def main():
     plainText = 'attackpostponed'
-    key = 41532
-    RTC(plainText, key)
+    key = 213
+    print(RTC(plainText, key))
 
 if __name__ == '__main__':
     main()
